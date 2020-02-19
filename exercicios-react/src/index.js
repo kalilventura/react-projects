a6d5a3b03119d7227293fe19c87dc0b9323ef8d6
kalilventura <kalilventur@gmail.com>
2020-02-19 15:32:31 -0300

    Trabalhando com mais de um componente

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
+import { CompA, CompB as B } from './components/SegundoComponenete'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
         <div>
             <PrimeiroComponente valor="Olá mundo"></PrimeiroComponente>
+            <CompA valor = "Olá, eu sou o Componente A" />
+            <B valor = "Olá, eu sou o Componente B" />
         </div>, elemento)
 
