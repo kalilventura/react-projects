89cd09875dbf6df792e50952df45e66df72fc06e
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-20 22:38:31 -0300

    Retornando multiplos Elementos

---
+++ exercicios-react/src/components/MultiElementos.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+
+// Solução 03
+export default props => [
+    <h1>Parte 01</h1>,
+    <h2>Parte 02</h2>
+]
+
+// Solução 02
+// export default props => {
+//     <React.Fragment>
+//         <h1>Parte 01</h1>
+//         <h2>Parte 02</h2>
+//     </React.Fragment>
+// }
+
+// Solução 01 -  Solução mais utilizada !!
+// export default props => {
+//     <div>
+//         <h1>Parte 01</h1>
+//         <h2>Parte 02</h2>
+//     </div>
+// }
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
 import { CompA, CompB as B } from './components/SegundoComponenete'
+import MultiElementos from './components/MultiElementos'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
-        <div>
-            <PrimeiroComponente valor="Olá mundo"></PrimeiroComponente>
-            <CompA valor = "Olá, eu sou o Componente A" />
-            <B valor = "Olá, eu sou o Componente B" />
-        </div>, elemento)
+    <div>
+        <MultiElementos />
+        <PrimeiroComponente valor="Olá mundo"></PrimeiroComponente>
+        <CompA valor="Olá, eu sou o Componente A" />
+        <B valor="Olá, eu sou o Componente B" />
+    </div>, elemento)
 
