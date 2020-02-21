947c19f8f8f1aca70f50bb507321ea8cbd656e9a
kalilventura <kalilventur@gmail.com>
2020-02-21 09:17:22 -0300

    Relacao entre componentes

--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import PrimeiroComponente from './components/PrimeiroComponente'
 import MensagemPadrao, { CompA, CompB as B } from './components/SegundoComponenete'
 import MultiElementos from './components/MultiElementos'
+import FamiliaSilva from './components/FamiliaSilva'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
@@ -12,5 +13,6 @@ ReactDOM.render(
         <CompA valor="Olá, eu sou o Componente A" />
         <B valor="Olá, eu sou o Componente B" />
         <MensagemPadrao valor="Eu sou uma função anonima exportada por 'Default' :) " />
+        <FamiliaSilva />
     </div>, elemento)
 
