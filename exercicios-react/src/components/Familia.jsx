cb260a7048bf7e958942060f23e0f9ebc0947426
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-02-24 21:51:27 -0300

    Propagando props para componentes filhos

--- exercicios-react/src/components/Familia.jsx
+++ exercicios-react/src/components/Familia.jsx
@@ -3,5 +3,8 @@ import React from 'react'
 export default props =>
     <div>
         <h1>Familia</h1>
-        {props.children}
+        {React.cloneElement(props.children, { ...props })}
+        {/* {React.cloneElement(props.children, props)} */}
+        {/* {React.cloneElement(props.children, { sobrenome: props.sobrenome })} */}
+        {/* {props.children} */}
     </div>
--- exercicios-react/src/index.js
+++ exercicios-react/src/index.js
@@ -3,28 +3,24 @@ import ReactDOM from 'react-dom'
 // import PrimeiroComponente from './components/PrimeiroComponente'
 // import MensagemPadrao, { CompA, CompB as B } from './components/SegundoComponenete'
 // import MultiElementos from './components/MultiElementos'
-import FamiliaSilva from './components/FamiliaSilva'
+// import FamiliaSilva from './components/FamiliaSilva'
 import Familia from './components/Familia'
 import Membro from './components/Membro'
 
 const elemento = document.getElementById('root')
 ReactDOM.render(
     <div>
-        <Familia>
-            <Membro nome ="Andre" sobrenome ="Pereira" />
-            <Membro nome ="Gustavo" sobrenome ="Pereira" />
+        <Familia sobrenome="Pereira">
+            <Membro nome="Andre" />
+            {/* <Membro nome="Gustavo" sobrenome="Pereira" /> */}
         </Familia>
-        <Familia >
-            <Membro nome ="Xavier" sobrenome ="da Silva" />
-            <Membro nome ="Antônio" sobrenome ="da Silva" />
-        </Familia>
-        <FamiliaSilva sobrenome="Silva Pereira"/>
+        {/* <FamiliaSilva sobrenome="Silva Pereira"/> */}
         {/* <MultiElementos />
         <PrimeiroComponente valor="Olá mundo"></PrimeiroComponente>
         <CompA valor="Olá, eu sou o Componente A" />
         <B valor="Olá, eu sou o Componente B" />
         <MensagemPadrao valor="Eu sou uma função anonima exportada por 'Default' :) " />
         <FamiliaSilva /> */}
-        
+
     </div>, elemento)
 
