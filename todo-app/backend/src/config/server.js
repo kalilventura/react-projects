e8547e6c08dbc5cee456fdb35006ff9df4e6990f
kalilventura <kalilventur@gmail.com>
2020-03-04 12:16:19 -0300

    Configuracao do express

---
+++ todo-app/backend/src/config/server.js
@@ -0,0 +1,12 @@
+const port = 3303
+
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log('Server is running at port '+ port)
+})
