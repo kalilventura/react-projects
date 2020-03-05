495f0615051560d126b6ca97be866b920778b265
Kalil Teixeira Ventura Monteiro <kalilventur@gmail.com>
2020-03-05 11:32:45 -0300

    Melhorando o layout

--- todo-app/frontend/todo/src/todo/todo.jsx
+++ todo-app/frontend/todo/src/todo/todo.jsx
@@ -4,11 +4,21 @@ import TodoForm from "./todoForm";
 import TodoList from "./todoList";
 
 export default class Todo extends Component {
+  constructor(props) {
+    super(props);
+    // O this sempre será a classe Todo
+    this.handleAdd = this.handleAdd.bind(this);
+  }
+
+  handleAdd() {
+    console.log(this);
+  }
+
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
-        <TodoForm />
+        <TodoForm handleAdd={this.handleAdd} />
         <TodoList />
       </div>
     );
